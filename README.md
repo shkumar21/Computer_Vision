d58dfdc60f9957c09305d3e7616b158eb16804d3
SHAILESH KUMAR <81655547+shkumar21@users.noreply.github.com>
2022-08-07 18:32:28 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Computer_Vision
-Image-Classification-using-CNN-for-Imagenette-Imagewoof-detaset-main
+# Image-Classification-using-CNN-for-Imagenette-Imagewoof-detaset
+In this project we tried to classify two data set by Cpnvolution Newral Network (CNN) using Keras &amp; tenser flow: 
+10 class problem, two different data sets, to be found at https:// github.com/fastai/imagenette
+Imagenette : 10 classes, fairly simple to be separated from the 1000 class imagenet challenge
+Imagewoof : 10 classes from imagenet depicting different dog breeds
+Datasets contain separate training (train) and validation ( val ) folders
+Programming Language: Python with TensorFlow, CNN and Keras etc.
+Feature Extraction Techniques: Convolutional Neural Network (CNN) with maxpooling and Batch normalization layers on sequential model using keras and Tensorflow python libraries.
+Classification Method: ANN and Dropout layers with sequential model using keras and Tensorflow python libraries.
+We compared the results from each feature extraction and classification techniques to evaluate the model on the basis of overall Computation time and accuracy and finally achieved an accuracy of 46%
---
+++ img_classifier.py
@@ -0,0 +1,66 @@
+imagenette_map = { 
+    "n01440764" : "tench",
+    "n02102040" : "springer",
+    "n02979186" : "casette_player",
+    "n03000684" : "chain_saw",
+    "n03028079" : "church",
+    "n03394916" : "French_horn",
+    "n03417042" : "garbage_truck",
+    "n03425413" : "gas_pump",
+    "n03445777" : "golf_ball",
+    "n03888257" : "parachute"
+}
+
+from keras.preprocessing.image import ImageDataGenerator
+
+# create a new generator
+imagegen = ImageDataGenerator()
+# load train data
+train = imagegen.flow_from_directory(r'D:/study/5th Semester/Computer Vision and Deep learning/New Material/Project and exercises/imagenette2-160/train/', class_mode="categorical", shuffle=False, batch_size=128, target_size=(224, 224))
+# load val data
+val = imagegen.flow_from_directory(r'D:/study/5th Semester/Computer Vision and Deep learning/New Material/Project and exercises/imagenette2-160/val/', class_mode="categorical", shuffle=False, batch_size=128, target_size=(224, 224))
+
+
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPool2D, Flatten, Dense, InputLayer, BatchNormalization, Dropout
+
+# build a sequential model
+model = Sequential()
+model.add(InputLayer(input_shape=(224, 224, 3)))
+
+# 1st conv block
+model.add(Conv2D(25, (5, 5), activation='relu', strides=(1, 1), padding='same'))
+model.add(MaxPool2D(pool_size=(2, 2), padding='same'))
+
+# 2nd conv block
+model.add(Conv2D(50, (5, 5), activation='relu', strides=(2, 2), padding='same'))
+model.add(MaxPool2D(pool_size=(2, 2), padding='same'))
+model.add(BatchNormalization())
+
+# 3rd conv block
+model.add(Conv2D(70, (3, 3), activation='relu', strides=(2, 2), padding='same'))
+model.add(MaxPool2D(pool_size=(2, 2), padding='valid'))
+model.add(BatchNormalization())
+
+# ANN block
+model.add(Flatten())
+model.add(Dense(units=100, activation='relu'))
+model.add(Dense(units=100, activation='relu'))
+model.add(Dropout(0.25))
+
+# output layer
+model.add(Dense(units=10, activation='softmax'))
+model.summary()
+
+# compile model
+model.compile(loss='categorical_crossentropy', optimizer="adam", metrics=['accuracy'])
+
+# fit on data for 30 epochs
+model.fit_generator(train, epochs=30, validation_data=val)
+
+# Save the model to disk.
+model.save_weights('img_cla')
+
+# extract train and val features
+features_train = model.predict(train)
+features_val = model.predict(val)
